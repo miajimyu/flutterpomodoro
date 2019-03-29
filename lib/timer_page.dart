456f2fae1676f20d46b82992bf8ecdf0d50a12e7
miajimyu <38499336+miajimyu@users.noreply.github.com>
2019-03-29 22:24:26 +0900

    pomodoro timer

--- lib/timer_page.dart
+++ lib/timer_page.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 
 import 'dart:async';
 
+final workTime = Duration(minutes: 25);
+final shortBreakTime = Duration(minutes: 5);
+
+// final workTime = Duration(seconds: 5);
+// final shortBreakTime = Duration(seconds: 2);
+
+enum TimerStatus {
+  work,
+  shortBreak,
+  longBreak,
+}
+
+class Pomodoro {
+  Duration targetTime;
+  TimerStatus timerStatus;
+  int count;
+
+  Pomodoro({
+    this.targetTime,
+    this.timerStatus,
+    this.count,
+  });
+}
+
 class Page extends StatefulWidget {
   @override
   _PageState createState() => _PageState();
@@ -10,16 +34,17 @@ class Page extends StatefulWidget {
 class _PageState extends State<Page> {
   Stopwatch _sw;
   Timer _timer;
-  Duration elapsed = Duration();
+  Duration _timeLeft = Duration();
 
-  final workTime = Duration(minutes: 25);
-  final breakTime = Duration(minutes: 5);
-
-  // final workTime = Duration(seconds: 5);
-  // final breakTime = Duration(seconds: 2);
+  Pomodoro _pomodoro = Pomodoro(
+    targetTime: workTime,
+    timerStatus: TimerStatus.work,
+    count: 0,
+  );
 
   @override
   void initState() {
+    _pomodoro.targetTime = workTime;
     _sw = Stopwatch();
     _timer = Timer.periodic(Duration(milliseconds: 30), _callback);
     super.initState();
@@ -33,16 +58,30 @@ class _PageState extends State<Page> {
   }
 
   void _callback(Timer timer) {
-    print('callback');
-    if (_sw.elapsed > workTime) {
-      _sw.stop();
+    var _newTimeLeft = _pomodoro.targetTime - _sw.elapsed;
+    if (_newTimeLeft != _timeLeft) {
+      setState(() {
+        _timeLeft = _newTimeLeft;
+      });
     }
 
-    var newElapsed = workTime - _sw.elapsed;
-    if (newElapsed != elapsed) {
+    if (_sw.elapsed > _pomodoro.targetTime) {
+      _sw.stop();
       setState(() {
-        elapsed = newElapsed;
+        _changeNextStatus();
       });
+    }
+  }
+
+  void _changeNextStatus() {
+    _sw.reset();
+    if (_pomodoro.timerStatus == TimerStatus.work) {
+      _pomodoro.targetTime = shortBreakTime;
+      _pomodoro.timerStatus = TimerStatus.shortBreak;
+      _pomodoro.count++;
+    } else {
+      _pomodoro.targetTime = workTime;
+      _pomodoro.timerStatus = TimerStatus.work;
     }
   }
 
@@ -61,48 +100,56 @@ class _PageState extends State<Page> {
   }
 
   String getTimeString() {
-    String minutes = (elapsed.inMinutes % 60).toString().padLeft(2, '0');
-    String seconds = (elapsed.inSeconds % 60).toString().padLeft(2, '0');
+    String minutes = (_timeLeft.inMinutes % 60).toString().padLeft(2, '0');
+    String seconds = (_timeLeft.inSeconds % 60).toString().padLeft(2, '0');
     return ("$minutes:$seconds");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Text(
-            getTimeString(),
-            style: TextStyle(fontSize: 72.0),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Ink(
-                decoration: ShapeDecoration(
-                  color: Colors.blueAccent,
-                  shape: CircleBorder(),
+    return Container(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              getTimeString(),
+              style: TextStyle(fontSize: 72.0),
+            ),
+            Text(
+              _pomodoro.count.toString(),
+              style: TextStyle(fontSize: 32.0),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Ink(
+                  decoration: ShapeDecoration(
+                    color: Colors.blueAccent,
+                    shape: CircleBorder(),
+                  ),
+                  child: IconButton(
+                    icon: Icon(_sw.isRunning ? null : Icons.autorenew),
+                    color: Colors.white,
+                    iconSize: 72,
+                    onPressed: () => _resetButtonPressed(),
+                  ),
                 ),
-                child: IconButton(
-                  icon: Icon(_sw.isRunning ? null : Icons.autorenew),
-                  color: Colors.white,
-                  onPressed: () => _resetButtonPressed(),
+                Ink(
+                  decoration: ShapeDecoration(
+                    color: Colors.blueAccent,
+                    shape: CircleBorder(),
+                  ),
+                  child: IconButton(
+                    icon: Icon(_sw.isRunning ? Icons.pause : Icons.play_arrow),
+                    color: Colors.white,
+                    iconSize: 72,
+                    onPressed: () => _buttonPressed(),
+                  ),
                 ),
-              ),
-              Ink(
-                decoration: ShapeDecoration(
-                  color: Colors.blueAccent,
-                  shape: CircleBorder(),
-                ),
-                child: IconButton(
-                  icon: Icon(_sw.isRunning ? Icons.pause : Icons.play_arrow),
-                  color: Colors.white,
-                  onPressed: () => _buttonPressed(),
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
